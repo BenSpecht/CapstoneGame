3c525875fc346e0e24e77dd5afc91cf05f5c5e7e
Dom Willmott <domwillmott@gmail.com>
2020-10-09 13:47:36 +1100

    added music and ambience with colliders - added snake hiss to animation

---
+++ Assets/FMOD_Animations.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FMOD_Animations : MonoBehaviour
+{
+    
+    void PlayScorpionCat_Meow(string path)
+    {
+
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+
+    }
+
+    void PlayFlowerSnake_Hiss(string path)
+    {
+
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+
+    }
+
+}
+    
