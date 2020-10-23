2a366817914e22fbb798d0025261fb18f21a854a
Aidan <leftaidan@gmail.com>
2020-10-24 01:08:41 +1100

    Moth interaction working and debug book added to dark world
    Moth works now, added a book down there for testing

--- Assets/Scripts/AnimalBehaviours/MothManager.cs
+++ Assets/Scripts/AnimalBehaviours/MothManager.cs
@@ -7,12 +7,17 @@ public class MothManager : MonoBehaviour
 {
     public GameManager gameManager;
     public TextMeshPro thinkingText;
+
+    private Animator anim;
+    
     
 
     // Start is called before the first frame update
     void Start()
     {
         thinkingText = FindObjectOfType<TextMeshPro>();
+
+        anim = gameObject.GetComponent <Animator>();
     }
 
     // Update is called once per frame
@@ -28,9 +33,14 @@ public class MothManager : MonoBehaviour
         if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
         {
             thinkingText.text = " ";
+            
+            anim.SetTrigger("fly");
+            gameManager.AddMothToBook();
+            
+            Debug.Log("FLY BASTARD");
             //moth.GetComponent<Animator>().Play("Moth_fly_away");
             //Julia doesn't know what she's doing, plz help her XD
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
              //MAKE MOTH BECOME ULTIMATE LIFEFORM
         }
     }
