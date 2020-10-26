e186d93ac05422c1a6bb4962b6b46e2ffa145613
Ben Specht <s3604514@student.rmit.edu.au>
2020-10-27 10:22:30 +1100

    Octopus Jar

--- Assets/Scripts/AnimalBehaviours/OctopusRadiusManager.cs
+++ Assets/Scripts/AnimalBehaviours/OctopusRadiusManager.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class OctopusRadiusManager : MonoBehaviour
 {
     public GameManager gameManager;
 
     public GameObject octopus;
+    public GameObject octopusFilled;
+    public GameObject otherOctopus;
     
     // Start is called before the first frame update
     void Start()
@@ -32,6 +35,10 @@ public class OctopusRadiusManager : MonoBehaviour
                 octopus.SetActive(false);
                 gameManager.AddOctopusToBook();
                 gameManager.bools.AnimalsMetBools.OctopusMet = true;
+                octopusFilled.SetActive(true);
+                otherOctopus.SetActive(false);
+                octopusFilled.transform.position = other.transform.position;
+                other.gameObject.SetActive(false);
             }
         }
     }
