97ad0e91087cd9abef95ce45df039c3109d27717
Aidan <leftaidan@gmail.com>
2020-10-26 18:00:49 +1100

    Menu working

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+
+    public void ButtonPlay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+    }
+
+    public void Credit()
+    {
+        
+        
+        
+    }
+
+    public void ButtonQuit()
+    {
+        Debug.Log("LET ME OUT");
+        Application.Quit();
+        
+    }
+}
